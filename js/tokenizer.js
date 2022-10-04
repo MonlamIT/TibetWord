c8a1aea259b332fc8e1b38b8c445d4ec7f8f8e42
Monlam IT <mlbodyig@gmail.com>
2022-10-04 20:01:39 +0530

    Update tokenizer.js

--- js/tokenizer.js
+++ js/tokenizer.js
@@ -1,6 +1,6 @@
 var dictionaryNames = ["tdict-city.txt", "tdict-collection.txt", "tdict-common.txt", "tdict-country.txt", "tdict-district.txt", "tdict-geo.txt", "tdict-history.txt", "tdict-ict.txt", "tdict-lang-ethnic.txt", "tdict-proper.txt", "tdict-science.txt", "tdict-spell.txt", "tdict-std-compound.txt", "tdict-std.txt"];
 var compoundDictionryNames = ["compound-words.txt"];
-var dictionaryURL = 'data/';
+var dictionaryURL = './data/';
 
 var compoundWords;
 var thaiWords;
